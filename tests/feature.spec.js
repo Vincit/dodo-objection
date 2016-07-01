e294504cefef7d10d1e74e52ab30478ed082f53e
Mikael Lepistö <mikael.lepisto@vincit.fi>
2016-07-01 10:54:01 +0300

    Added travis configuration and updated version to 0.0.4, need to finish writing tests soon

--- tests/feature.spec.js
+++ tests/feature.spec.js
@@ -6,24 +6,24 @@ var sinon = require('sinon');
 describe('Feature initialization', function () {
 
   // TODO: somthing like this:
-  // var featureInstance = new ObjectionFeature(sinon.spy(), sinon.spy()); 
+  // var featureInstance = new ObjectionFeature(sinon.spy(), sinon.spy());
 
-  // TODO: getDBConfig should require parameter when function 
+  // TODO: getDBConfig should require parameter when function
   //       passed as configuration
 
   // TODO: should cache DB instance if called twice with same config
 
-  // TODO: should have knex(req?) function to get knex instance 
+  // TODO: should have knex(req?) function to get knex instance
 
-  // TODO: should have disconnectKnex() function to destroy all databases 
+  // TODO: should have disconnectKnex() function to destroy all databases
 
   // TODO: should models(req?) which returns all models bound with connection
 
   // TODO: should bind all models found from directory to given connection
 
-  // TODO: should register middleware, which adds models and knex connection to request 
+  // TODO: should register middleware, which adds models and knex connection to request
 
-  // TODO: should configure postgres nicely to return timestamps and dates as strings, 
+  // TODO: should configure postgres nicely to return timestamps and dates as strings,
   //       bigintegers as numbers and json as string
 
   it('should run this test', function () {
